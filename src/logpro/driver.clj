25e67695f6ff37db514175c4eac42845947750d7
Ilia-Luchnikov <luchnikovilya@gmail.com>
2025-09-07 21:21:31 +0400

    matching and unification separated

--- src/logpro/driver.clj
+++ src/logpro/driver.clj
@@ -1,98 +1,17 @@
 (ns logpro.driver
   (:require [clojure.edn :as edn]
             [logpro.exprs :refer
-             [variable? compound-expr? empty-expr? get-expr-head get-expr-tail
-              vararg-head? get-varargs is-query? and-query? or-query? rule? get-and-body
+             [variable? is-query? and-query? or-query? rule? get-and-body
               get-or-body get-is-lhs get-is-rhs mangle-rule get-rule-body
-              get-conclusion not-query? get-not-body clojure-pred-query? get-clojure-pred-body
-              assertion? get-assertion-body unmangle-variable]]
+              get-conclusion not-query? get-not-body clojure-pred-query?
+              get-clojure-pred-body assertion? get-assertion-body unmangle-variable]]
             [logpro.frames :refer
-             [get-binding insert-binding invalid-frame? invalid-frame
-              filter-invalid-frames get-single-elem-stream empty-frames-stream
-              flatmap instantiate instantiate-stream init-frames-stream]]
-            [logpro.db :refer [fetch-rules fetch-assertions add-rule add-assertion]]))
-
-;; pattern matching
-
-(declare match)
-
-(defn extend-if-consistent [pat dat frame]
-  (let [val (get-binding frame pat)]
-    (if val
-      (match val dat frame)
-      (insert-binding frame pat dat))))
-
-(defn match-compounds [pat dat frame]
-  (cond
-    (and (empty-expr? pat) (empty-expr? dat)) frame
-    (and (vararg-head? pat) (vararg-head? dat)) (match (get-varargs pat) (get-varargs dat) frame)
-    (vararg-head? pat) (match (get-varargs pat) dat frame)
-    (vararg-head? dat) (match pat (get-varargs dat) frame)
-    (or (empty-expr? pat) (empty-expr? dat)) invalid-frame
-    :else (->> frame
-               (match (get-expr-head pat) (get-expr-head dat))
-               (match (get-expr-tail pat) (get-expr-tail dat)))))
-
-(defn match [pat dat frame]
-  (cond
-    (invalid-frame? frame) frame
-    (variable? pat) (extend-if-consistent pat dat frame)
-    (and (compound-expr? pat) (compound-expr? dat)) (match-compounds pat dat frame)
-    (= pat dat) frame
-    :else invalid-frame))
-
-;; unification
-
-(defn depends-on? [val var frame]
-  (cond
-    (variable? val) (if (= val var)
-                      true
-                      (let [val-lhs (get-binding frame val)]
-                        (if val-lhs
-                          (depends-on? val-lhs var frame)
-                          false)))
-    (compound-expr? val) (if (empty-expr? val)
-                           false
-                           (or
-                            (depends-on? (get-expr-head val) var frame)
-                            (depends-on? (get-expr-tail val) var frame)))
-    :else false))
-
-(declare unify)
-
-(defn extend-if-possible [var val frame]
-  (let [val-lhs (get-binding frame var)]
-    (cond
-      val-lhs (unify val-lhs val frame)
-      (variable? val) (let [val-rhs (get-binding frame val)]
-                        (if val-rhs
-                          (unify var val-rhs frame)
-                          (insert-binding frame var val)))
-      (depends-on? val var frame) invalid-frame
-      :else (insert-binding frame var val))))
-
-(defn unify-compounds [lhs-dat rhs-dat frame]
-  (cond
-    (and (empty-expr? lhs-dat) (empty-expr? rhs-dat)) frame
-    (and (vararg-head? lhs-dat) (vararg-head? rhs-dat)) (unify
-                                                         (get-varargs lhs-dat)
-                                                         (get-varargs rhs-dat)
-                                                         frame)
-    (vararg-head? lhs-dat) (unify (get-varargs lhs-dat) rhs-dat frame)
-    (vararg-head? rhs-dat) (unify lhs-dat (get-varargs rhs-dat) frame)
-    (or (empty-expr? lhs-dat) (empty-expr? rhs-dat)) invalid-frame
-    :else (->> frame
-               (unify (get-expr-head lhs-dat) (get-expr-head rhs-dat))
-               (unify (get-expr-tail lhs-dat) (get-expr-tail rhs-dat)))))
-
-(defn unify [p1 p2 frame]
-  (cond
-    (invalid-frame? frame) frame
-    (variable? p1) (extend-if-possible p1 p2 frame)
-    (variable? p2) (extend-if-possible p2 p1 frame)
-    (and (compound-expr? p1) (compound-expr? p2)) (unify-compounds p1 p2 frame)
-    (= p1 p2) frame
-    :else invalid-frame))
+             [invalid-frame? filter-invalid-frames get-single-elem-stream
+              empty-frames-stream flatmap instantiate instantiate-stream
+              init-frames-stream]]
+            [logpro.db :refer [fetch-rules fetch-assertions add-rule add-assertion]]
+            [logpro.matching :refer [match]]
+            [logpro.unification :refer [unify]]))
 
 ;; eval primitives
 
---
+++ src/logpro/unification.clj
@@ -0,0 +1,56 @@
+(ns logpro.unification
+  (:require
+   [logpro.exprs :refer [variable? get-expr-head get-expr-tail empty-expr?
+                         compound-expr? vararg-head? get-varargs]]
+   [logpro.frames :refer [get-binding insert-binding invalid-frame invalid-frame?]]))
+
+(defn depends-on? [val var frame]
+  (cond
+    (variable? val) (if (= val var)
+                      true
+                      (let [val-lhs (get-binding frame val)]
+                        (if val-lhs
+                          (depends-on? val-lhs var frame)
+                          false)))
+    (compound-expr? val) (if (empty-expr? val)
+                           false
+                           (or
+                            (depends-on? (get-expr-head val) var frame)
+                            (depends-on? (get-expr-tail val) var frame)))
+    :else false))
+
+(declare unify)
+
+(defn extend-if-possible [var val frame]
+  (let [val-lhs (get-binding frame var)]
+    (cond
+      val-lhs (unify val-lhs val frame)
+      (variable? val) (let [val-rhs (get-binding frame val)]
+                        (if val-rhs
+                          (unify var val-rhs frame)
+                          (insert-binding frame var val)))
+      (depends-on? val var frame) invalid-frame
+      :else (insert-binding frame var val))))
+
+(defn unify-compounds [lhs-dat rhs-dat frame]
+  (cond
+    (and (empty-expr? lhs-dat) (empty-expr? rhs-dat)) frame
+    (and (vararg-head? lhs-dat) (vararg-head? rhs-dat)) (unify
+                                                         (get-varargs lhs-dat)
+                                                         (get-varargs rhs-dat)
+                                                         frame)
+    (vararg-head? lhs-dat) (unify (get-varargs lhs-dat) rhs-dat frame)
+    (vararg-head? rhs-dat) (unify lhs-dat (get-varargs rhs-dat) frame)
+    (or (empty-expr? lhs-dat) (empty-expr? rhs-dat)) invalid-frame
+    :else (->> frame
+               (unify (get-expr-head lhs-dat) (get-expr-head rhs-dat))
+               (unify (get-expr-tail lhs-dat) (get-expr-tail rhs-dat)))))
+
+(defn unify [p1 p2 frame]
+  (cond
+    (invalid-frame? frame) frame
+    (variable? p1) (extend-if-possible p1 p2 frame)
+    (variable? p2) (extend-if-possible p2 p1 frame)
+    (and (compound-expr? p1) (compound-expr? p2)) (unify-compounds p1 p2 frame)
+    (= p1 p2) frame
+    :else invalid-frame))
